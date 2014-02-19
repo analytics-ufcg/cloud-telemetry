c31e3e19df07049898cc7c6a4f32f13ba3aec444
Iury Gregory Melo Ferreira <iury@regressao.(none)>
2014-02-19 14:48:30 -0300

    Montagem URL

---
+++ static/js/scriptsUI/alarms.js
@@ -0,0 +1,25 @@
+var html_alarm = '<div class="row"><div class="col-lg-8">Nome do alarme:<input type="text" name="alarm_name"></input></div>';
+html_alarm += '<div class="col-lg-4"><span>Tipo </span><select id="mytypes" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
+html_alarm += '<option value="" label=""></option><option value="cpu" label="cpu">cpu</option></select></div></div>';
+html_alarm += '<div class="row"><div class="col-lg-8">Parâmetro<input type="text" name="param_val"></input></div>';
+html_alarm += '<div class="col-lg-4"><span>Operador: </span><select id="mytypes" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
+html_alarm += '<option value="" label=""></option><option value="maior" label="&gt;"></option><option value="maiorque" label="&ge;"></option>';
+html_alarm += '<option value="menor" label="&lt;"></option><option value="menorque" label="&le;"></option><option value="igual" label="&#x3d;"></option>';
+html_alarm += '</select></div></div>';
+/*Funcao que oferece a adição de alarme*/
+function addAlarme() {
+	bootbox.dialog({
+
+		message : html_alarm,
+		title : "Adição de Alarmes",
+		buttons : {
+			main : {
+				label : "Adicionar",
+				className : "btn-primary",
+				callback : function() {
+					console.log("Hi " + $('#first_name').val());
+				}
+			}
+		}
+	});
+}
--- static/js/scriptsUI/menuconfig.js
+++ static/js/scriptsUI/menuconfig.js
@@ -1,3 +1,5 @@
+var dados;
+
 /* Habilitando seletores de data/hora */
 $('#datetimepicker1').datetimepicker({
 	pick12HourFormat : true
@@ -15,12 +17,28 @@ $('.input-group-addon').click(function() {
 	$('input[name=defaultTime]').prop('checked', false);
 });
 
+/*Funcao para converter dia e mês*/
+function formattedDate(date) {
+    var d = new Date(date || Date.now()),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [day, month, year].join('/');
+}
+
 $('#aplicarConf').click(function() {
 	var out = $("input[name='defaultTime']:checked").val();
 	var dh1 = $('#data_hora1').val().replace("/", " ").replace("/", " ").replace(":", " ").split(" ");
 	var dt1 = new Date(dh1[2], dh1[1], dh1[0], dh1[3], dh1[4]);
 	var dh2 = $('#data_hora2').val().replace("/", " ").replace("/", " ").replace(":", " ").split(" ");
 	var dt2 = new Date(dh2[2], dh2[1], dh2[0], dh2[3], dh2[4]);
+	console.log($('#data_hora1').val());
+	console.log($('#data_hora2').val());
+
 	/*Verificações antes de realizar requisição*/
 	var html_m = '<h2>Atenção!</h2><br />';
 	/*Nenhum Campo selecionado*/
@@ -28,11 +46,6 @@ $('#aplicarConf').click(function() {
 		html_m += '<h4>Nenhuma opção selecionada, escolha uma das opções de <b>Período Fixo</b> \n ou <b> Período Específico</b>  </h4><br />';
 		bootbox.alert(html_m);
 	}
-	/*Verificacao de Datas futuras*/
-	var dtsys = new Date();
-	var atual = new Date(dtsys.getFullYear(), dtsys.getMonth() + 1, dtsys.getDay(), dtsys.getHours(), dtsys.getMinutes());
-	console.log(dt1.getTime() - atual.getTime() > 60);
-	console.log();
 	/*Data de Inicio maior igual Data Fim*/
 	if (dt1.getTime() >= dt2.getTime()) {
 		html_m += '<h4>A data de início fornecida possui tempo maior ou igual à data fim.</h4><br />';
@@ -40,6 +53,44 @@ $('#aplicarConf').click(function() {
 		bootbox.alert(html_m);
 	}
 
+
+	var now = new Date();
+	now.setTime( now.getTime() + now.getTimezoneOffset() );
+	var url_requisicao = "/cpu_util?";
+	if (out == "ultima_hora") {
+		var ontem = new Date(now - (1000 * 60 * 60 * 1));
+		ontem.setTime( ontem.getTime() + ontem.getTimezoneOffset() );
+		console.log(ontem.getDate());
+		url_requisicao += "start_date=" + formattedDate(ontem) + " " +ontem.getHours() + ":" + ontem.getMinutes() ;
+		url_requisicao += "&end_date" + formattedDate(now) + " " +now.getHours() + ":" + now.getMinutes() ;
+		console.log(url_requisicao);
+	} else if (out == "ultimo_dia") {
+		var ontem = new Date(now - (1000 * 60 * 60 * 24 * 1));
+		ontem.setTime( ontem.getTime() + ontem.getTimezoneOffset() );
+		url_requisicao += "start_date=" + formattedDate(ontem) + " " +ontem.getHours() + ":" + ontem.getMinutes() ;
+		url_requisicao += "&end_date" + formattedDate(now) + " " +now.getHours() + ":" + now.getMinutes() ;		
+		console.log(url_requisicao);
+	} else if (out == "ultima_semana") {
+		var ontem = new Date(now - (1000 * 60 * 60 * 24 * 7));
+		ontem.setTime( ontem.getTime() + ontem.getTimezoneOffset() );
+		url_requisicao += "start_date=" + formattedDate(ontem) + " " +ontem.getHours() + ":" + ontem.getMinutes() ;
+		url_requisicao += "&end_date" + formattedDate(now) + " " +now.getHours() + ":" + now.getMinutes() ;
+		console.log(url_requisicao);
+	} else if (out == "ultimo_mes") {
+		var ontem = new Date(now - (1000 * 60 * 60 * 24 * 30));
+		ontem.setTime( ontem.getTime() + ontem.getTimezoneOffset() );
+		url_requisicao += "start_date=" + formattedDate(ontem) + " " +ontem.getHours() + ":" + ontem.getMinutes() ;
+		url_requisicao += "&end_date" + formattedDate(now) + " " +now.getHours() + ":" + now.getMinutes() ;
+		console.log(url_requisicao);
+	} else {
+		url_requisicao += "start_date=" + $('#data_hora1').val();
+		url_requisicao += "&end_date" + $('#data_hora2').val();
+	}
+
+	$.get(url_requisicao, function(data) {
+		dados = JSON.parse(data);
+		console.log(url_requisicao);
+	});
 });
 
 /* Habilitar div selecionada de acordo com a aba selecionada*/
@@ -89,29 +140,3 @@ function show_hist() {
 $(function() {
 	$("#myTab a:last").tab('show');
 });
-
-var html_alarm = '<div class="row"><div class="col-lg-8">Nome do alarme:<input type="text" name="alarm_name"></input></div>';
-html_alarm += '<div class="col-lg-4"><span>Tipo </span><select id="mytypes" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
-html_alarm += '<option value="" label=""></option><option value="cpu" label="cpu">cpu</option></select></div></div>';
-html_alarm += '<div class="row"><div class="col-lg-8">Parâmetro<input type="text" name="param_val"></input></div>';
-html_alarm += '<div class="col-lg-4"><span>Expressão: </span><select id="mytypes" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
-html_alarm += '<option value="" label=""></option><option value="maior" label="&gt;"></option><option value="maiorque" label="&ge;"></option>';
-html_alarm += '<option value="menor" label="&lt;"></option><option value="menorque" label="&le;"></option><option value="igual" label="&#x3d;"></option>';
-html_alarm += '</select></div></div>';
-/*Funcao que oferece a adição de alarme*/
-function addAlarme() {
-	bootbox.dialog({
-		
-		message : html_alarm,
-		title : "Adição de Alarmes",
-		buttons : {
-			main : {
-				label : "Adicionar",
-				className : "btn-primary",
-				callback : function() {
-					console.log("Hi " + $('#first_name').val());
-				}
-			}
-		}
-	});
-}
