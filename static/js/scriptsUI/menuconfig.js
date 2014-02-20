3d06d1855016c64a15c3d04d2039358735e2bd45
Iury Gregory Melo Ferreira <iury@dbscan.(none)>
2014-02-20 15:04:58 -0300

    Modificação na requisicao, mês apareceia como mês anterior.

--- static/js/scriptsUI/menuconfig.js
+++ static/js/scriptsUI/menuconfig.js
@@ -19,9 +19,15 @@ $('.input-group-addon').click(function() {
 
 /*Funcao para converter dia e mês*/
 function formattedDate(date, verificador) {
-	var d = new Date(date || Date.now()), month = '' + (d.getMonth()), day = '' + d.getDate(), year = d.getFullYear();
-	hour = '' + (d.getHours());
+	var d = new Date(date || Date.now()), 
+	month = '' + (d.getMonth()+1), 
+	day = '' + d.getDate(), 
+	year = ''+d.getFullYear(),
+	hour = '' + (d.getHours()),
 	minuto = '' + (d.getMinutes());
+	if(verificador == 1){
+		month = ''+d.getMonth();
+	}
 	if (month.length < 2)
 		month = '0' + month;
 	if (day.length < 2)
@@ -31,11 +37,8 @@ function formattedDate(date, verificador) {
 	if (minuto.length < 2)
 		minuto = '0' + minuto;
 
-	if ( verificador = "") {
-		return [year, month+1, day].join('-') + "T" + hour + ":" + minuto + ":00";
-	} else {
-		return [year, month, day].join('-') + "T" + hour + ":" + minuto + ":00";
-	}
+	return [year, month, day].join('-') + "T" + hour + ":" + minuto + ":00";
+
 }
 
 
@@ -67,30 +70,30 @@ $('#aplicarConf').click(function() {
 	if (out == "ultima_hora") {
 		var ontem = new Date(now - (1000 * 60 * 60 * 1));
 		ontem.setTime(ontem.getTime() + ontem.getTimezoneOffset());
-		url_requisicao += "timestamp_begin=" + formattedDate(ontem,"");
-		url_requisicao += "&timestamp_end=" + formattedDate(now,"");
+		url_requisicao += "timestamp_begin=" + formattedDate(ontem,0);
+		url_requisicao += "&timestamp_end=" + formattedDate(now,0);
 		console.log(url_requisicao);
 	} else if (out == "ultimo_dia") {
 		var ontem = new Date(now - (1000 * 60 * 60 * 24 * 1));
 		ontem.setTime(ontem.getTime() + ontem.getTimezoneOffset());
-		url_requisicao += "timestamp_begin=" + formattedDate(ontem,"");
-		url_requisicao += "&timestamp_end=" + formattedDate(now,"");
+		url_requisicao += "timestamp_begin=" + formattedDate(ontem,0);
+		url_requisicao += "&timestamp_end=" + formattedDate(now,0);
 		console.log(url_requisicao);
 	} else if (out == "ultima_semana") {
 		var ontem = new Date(now - (1000 * 60 * 60 * 24 * 7));
 		ontem.setTime(ontem.getTime() + ontem.getTimezoneOffset());
-		url_requisicao += "timestamp_begin=" + formattedDate(ontem,"");
-		url_requisicao += "&timestamp_end=" + formattedDate(now,"");
+		url_requisicao += "timestamp_begin=" + formattedDate(ontem,0);
+		url_requisicao += "&timestamp_end=" + formattedDate(now,0);
 		console.log(url_requisicao);
 	} else if (out == "ultimo_mes") {
 		var ontem = new Date(now - (1000 * 60 * 60 * 24 * 30));
 		ontem.setTime(ontem.getTime() + ontem.getTimezoneOffset());
-		url_requisicao += "timestamp_begin=" + formattedDate(ontem,"");
-		url_requisicao += "&timestamp_end=" + formattedDate(now,"");
+		url_requisicao += "timestamp_begin=" + formattedDate(ontem,0);
+		url_requisicao += "&timestamp_end=" + formattedDate(now,0);
 		console.log(url_requisicao);
 	} else {
-		url_requisicao += "timestamp_begin=" + formattedDate(dt1,"data1");
-		url_requisicao += "&timestamp_end=" + formattedDate(dt2,"data2");
+		url_requisicao += "timestamp_begin=" + formattedDate(dt1,1);
+		url_requisicao += "&timestamp_end=" + formattedDate(dt2,1);
 		console.log(url_requisicao);
 	}
 
