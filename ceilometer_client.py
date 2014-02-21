3d3d7489bc92d3e8b010ecbfbcdbd0977518cc4c
Paulo Romulo <p.romuloo@gmail.com>
2014-02-21 20:33:36 +0000

    Add new call /projects to retrieve projects and VMs with cpu_util number

--- ceilometer_client.py
+++ ceilometer_client.py
@@ -2,7 +2,8 @@ from ceilometerclient import client
 
 from keystone_client import KeystoneClient
 
-import json
+from time import gmtime, strftime
+import json, datetime
 
 API_VERSION = '2'
 OS_USERNAME = 'admin'
@@ -15,6 +16,26 @@ class CeilometerClient:
     def __init__(self):
         self.ceilometer = client.get_client(API_VERSION, os_username=OS_USERNAME, os_password=OS_PASSWORD, os_tenant_name=OS_TENANT_NAME, os_auth_url=OS_AUTH_URL)
         self.keystone = KeystoneClient(OS_USERNAME, OS_PASSWORD, OS_AUTH_URL)
+        #TERRIBLE - FIX THIS
+        d = datetime.datetime.now() + datetime.timedelta(seconds=-60*60)
+        self.last_data = self.ceilometer.samples.list('cpu_util', [{'field' : 'timestamp', 'op' : 'gt', 'value' : d.strftime("%Y-%m-%dT%H:%M:%S")}])
+
+    def get_projects_with_cpu_util(self):
+        projects = self.keystone.projects
+
+        ret = { 'name' : 'cloud', 'children' : [] }
+
+        for p in projects.keys():
+            proj = { 'name' : projects[p], 'children' : [] }
+
+            for d in self.last_data:
+                if d.project_id == p and not any(d.resource_id in k.values() for k in proj['children']):
+                    proj['children'].append({ 'resource_id' : d.resource_id, 'cpu_util_percent' : d.counter_volume })
+
+            ret['children'].append(proj)
+
+        return json.dumps(ret)
+
 
     def get_cpu_util(self, timestamp_begin=None, timestamp_end=None, resource_id=None):
         query = []
@@ -33,10 +54,14 @@ class CeilometerClient:
 
         ret = []
         for d in data:
-            ret.append(json.dumps({ 'resource_id' : d.resource_id, 'timestamp' : d.timestamp, 'cpu_util_percent' : d.counter_volume, 'project_id' : d.project_id, 'project_name' : self.keystone.projects.get(d.project_id) }))
+            ret.append({ 'resource_id' : d.resource_id, 'timestamp' : d.timestamp, 'cpu_util_percent' : d.counter_volume })
     
         return json.dumps(ret)
 
     def set_alarm(self, name, meter, threshold, operator, period, evaluation_period):
-        self.ceilometer.alarms.create(name=name, meter_name=meter, threshold=threshold, comparison_operator=operator, period=period, evaluation_periods=evaluation_period, alarm_actions='log://')
+        try:
+            alarm = self.ceilometer.alarms.create(name=name, meter_name=meter, threshold=threshold, comparison_operator=operator, period=period, evaluation_periods=evaluation_period, alarm_actions='log://')
+            return alarm is None
+        except:
+            return False
       
--- cloud_telemetry.py
+++ cloud_telemetry.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request, make_response
 
-#from ceilometer_client import CeilometerClient
+from ceilometer_client import CeilometerClient
 #from local_db_worker import db_worker
 
 import json
 
 app = Flask(__name__)
-#ceilometer = CeilometerClient()
+ceilometer = CeilometerClient()
 
 @app.route('/')
 def home(name=None):
     return render_template('index.html', name=name)
+
+@app.route('/projects')
+def projects():
+    resp = make_response(ceilometer.get_projects_with_cpu_util())
+    resp.headers['Access-Control-Allow-Origin'] = "*" 
+
+    return resp
 
 @app.route('/cpu_util')  
 def cpu_util():
@@ -18,19 +25,33 @@ def cpu_util():
     timestamp_end = request.args.get('timestamp_end', None)
     resource_id = request.args.get('resource_id', None)
     
-    #resp = make_response(ceilometer.get_cpu_util(timestamp_begin,timestamp_end,resource_id))
-    #resp.headers['Access-Control-Allow-Origin'] = "*" 
+    resp = make_response(ceilometer.get_cpu_util(timestamp_begin,timestamp_end,resource_id))
+    resp.headers['Access-Control-Allow-Origin'] = "*" 
 
-    #return resp
-    return json.dumps({'response' : 'echo', 'timestamp_begin' : timestamp_begin, 'timestamp_end' : timestamp_end, 'resource_id' : resource_id})
+    return resp
+    #return json.dumps({'response' : 'echo', 'timestamp_begin' : timestamp_begin, 'timestamp_end' : timestamp_end, 'resource_id' : resource_id})
 
 @app.route('/add_alarm',  methods=['POST'])
 def add_alarm():
+    name = request.args.get('name')
     resource = request.args.get('resource')
-    operation = request.args.get('operation')
+    operator = request.args.get('operator')
     threshold = request.args.get('threshold')
+    period = request.args.get('period')
 
-    return json.dumps({'response' : 'echo', 'resource' : resource, 'threshold' : threshold, 'operation' : operation})
+    #resp = make_response(json.dumps({'resource' : resource, 'threshold' : threshold, 'operation' : operation}))
+    
+    alarm = ceilometer.set_alarm(name, resource, threshold, operator, period, 1)
+    
+    if alarm:
+        resp = make_response(json.dumps({'alarm_id' : alarm.alarm_id}))
+    else:
+        resp = make_response(json.dumps({'alarm_id' : 'null'}))
+
+    resp.headers['Access-Control-Allow-Origin'] = "*" 
+
+    return resp
+    
 
 
 if __name__ == '__main__':
