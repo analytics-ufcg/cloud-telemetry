8aba58d47222f5dd22738d99c7e3d775860fd723
Paulo Romulo <p.romuloo@gmail.com>
2014-03-11 16:34:26 -0300

    Fix typo

--- view/html/telemetry_view.py
+++ view/html/telemetry_view.py
@@ -6,16 +6,12 @@ app = Flask(__name__)
 
 __SERVER_ADDRESS = 'http://150.165.15.4:9090'
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/projects/instances')
 def projects_instances():
     r = requests.get(__SERVER_ADDRESS + '/projects/instances')
     if r.status_code == 200:
         resp = make_response(r.json())
-    else
+    else:
         resp = make_response(json.dumps({ 'error' : r.status_code }))
 
     resp.headers['Access-Control-Allow-Origin'] = "*"
@@ -23,6 +19,5 @@ def projects_instances():
     return resp    
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=9999)
+    app.run(host='0.0.0.0', port=9090)
 
