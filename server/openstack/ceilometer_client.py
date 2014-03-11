88ff5d08d5d934e87187880dfd7c8a8922728283
Paulo Romulo <p.romuloo@gmail.com>
2014-03-11 17:19:03 +0000

    Small refactor on ceilometer client

--- server/openstack/ceilometer_client.py
+++ server/openstack/ceilometer_client.py
@@ -8,7 +8,7 @@ class CeilometerClient:
     def __init__(self):
         self.ceilometer = client.get_client(env.CEILOMETER_API_VERSION, os_username=env.OS_USERNAME, os_password=env.OS_PASSWORD, os_tenant_name=env.OS_TENANT_NAME, os_auth_url=env.OS_AUTH_URL)
 
-    def get_cpu_util(self, timestamp_begin=None, timestamp_end=None, resource_id=None, project_id=None):
+    def __get_cpu_util_raw(self, timestamp_begin=None, timestamp_end=None, resource_id=None, project_id=None):
         query = []
 
         if any([timestamp_begin, timestamp_end, resource_id, project_id]):
@@ -24,8 +24,10 @@ class CeilometerClient:
             if project_id:
                 query.append({'field':'project_id', 'op':'eq', 'value':project_id})
 
-        data = self.ceilometer.samples.list('cpu_util', query)
+        return self.ceilometer.samples.list('cpu_util', query)
 
+    def get_cpu_util(self, timestamp_begin=None, timestamp_end=None, resource_id=None, project_id=None):
+        data = self.__get_cpu_util_raw(timestamp_begin, timestamp_end, resource_id, project_id)
         ret = []
         for d in data:
             ret.append({ 'resource_id' : d.resource_id, 'timestamp' : d.timestamp, 'cpu_util_percent' : d.counter_volume })
@@ -33,22 +35,7 @@ class CeilometerClient:
         return ret
 
     def get_cpu_util_flavors(self, timestamp_begin=None, timestamp_end=None, resource_id=None, project_id=None):
-        query = []
-
-        if any([timestamp_begin, timestamp_end, resource_id, project_id]):
-            if timestamp_begin:
-                query.append({'field':'timestamp', 'op':'gt', 'value':timestamp_begin})
-
-            if timestamp_end:
-                query.append({'field':'timestamp', 'op':'lt', 'value':timestamp_end})
-
-            if resource_id:
-                query.append({'field':'resource_id', 'op':'eq', 'value':resource_id})
-
-            if project_id:
-                query.append({'field':'project_id', 'op':'eq', 'value':project_id})
-
-        data = self.ceilometer.samples.list('cpu_util', query)
+        data = self.__get_cpu_util_raw(timestamp_begin, timestamp_end, resource_id, project_id)
         ret = []
         for d in data:
             ret.append({'VM': d.resource_id, 'Cores': d.resource_metadata['flavor.vcpus'], 'CPU_UTIL': d.counter_volume})
