427d0c5ba5017e858c75b7217348debed5085a1d
Paulo Romulo <p.romuloo@gmail.com>
2014-03-14 16:55:58 -0300

    Also forward POST calls

--- view/html/telemetry_view.py
+++ view/html/telemetry_view.py
@@ -4,37 +4,46 @@ import requests, json
 
 app = Flask(__name__)
 
-__SERVER_ADDRESS = 'http://150.165.15.4:9090'
+__SERVER_ADDRESS = 'http://150.165.15.4:1313'
 
 @app.route('/projects')
 def projects():
-    return forward_get(request)
+    return forward_request(request)
 
 @app.route('/projects/instances')
 def projects_instances():
-    return forward_get(request)
+    return forward_request(request)
 
 @app.route('/cpu_util')
 def cpu_util():
-    return forward_get(request)
+    return forward_request(request)
 
 @app.route('/cpu_util_flavors')
 def cpu_util_flavors():
-    return forward_get(request)
+    return forward_request(request)
 
 @app.route('/alarms_history')
 def alarms_history():
-    return forward_get(request)
+    return forward_request(request)
+
+@app.route('/hosts_cpu_util')
+def hosts_cpu_util():
+    return forward_request(request)
 
 @app.route('/add_alarm',  methods=['POST'])
 def add_alarm():
-    pass
+    return forward_request(request)    
 
-def forward_get(req):
+def forward_request(req):
     url = req.url
     path = req.path
     print __SERVER_ADDRESS + path + url[url.find(path)+len(path):]
-    r = requests.get(__SERVER_ADDRESS + path + url[url.find(path)+len(path):])
+    r = None
+    if req.method == 'POST':
+        r = requests.post(__SERVER_ADDRESS + path + url[url.find(path)+len(path):])
+    else:
+        r = requests.get(__SERVER_ADDRESS + path + url[url.find(path)+len(path):])
+    
     if r.status_code == 200:
         resp = make_response(json.dumps(r.json()))
     else:
