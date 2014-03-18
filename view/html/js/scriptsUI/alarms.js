dbd5ef6e1935c9d5f3c9a2b08b88f9909c839a12
Iury Gregory Melo Ferreira <iury@regressao.(none)>
2014-03-18 12:01:25 -0300

    Remoção de parametros do operador, adição do parametro tempo

--- view/html/js/scriptsUI/alarms.js
+++ view/html/js/scriptsUI/alarms.js
@@ -1,17 +1,17 @@
 var retorno_alarm;
 
 //input para nome do alarme
-var html_addalarm = '<div class="row"><div class="col-lg-8">Nome do alarme:<input type="text" id="name_alarm" name="alarm_name"></input></div>';
+var html_addalarm = '<div class="row"><div class="col-lg-8 col-md-8">Nome do alarme:  <input type="text" id="name_alarm" name="alarm_name"></input></div>';
 //criacao de dropdown com os recursos
-html_addalarm += '<div class="col-lg-4"><span>Recurso</span><select id="myRecursos" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
+html_addalarm += '<div class="col-lg-4 col-md-4"><span>Recurso</span><select id="myRecursos" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
 html_addalarm += '<option value="" label=""></option><option value="cpu_util" label="cpu_util">cpu_util</option></select></div></div>';
 //valor de threshold
-html_addalarm += '<div class="row"><div class="col-lg-8">Threshold<input type="text" id="parametro" name="param_val"></input></div>';
+html_addalarm += '<div class="row"><div class="col-lg-8 col-md-8">Threshold  <input type="text" id="parametro" name="param_val"></input></div>';
 //criacao de dropdown para escolhar do operador
-html_addalarm += '<div class="col-lg-4"><span>Operador: </span><select id="myOp" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
-html_addalarm += '<option value="" label=""></option><option value="eq" label="igual">igual</option><option value="gt" label="maior">maior</option>';
-html_addalarm += '<option value="ge" label="maior igual">maior igual</option><option value="lt" label="menor">menor</option><option value="le" label"menor igual">menor igual</option>';
-html_addalarm += '</select></div></div>';
+html_addalarm += '<div class="col-lg-4 col-md-4"><span>Operador: </span><select id="myOp" class="selectpicker show-tick" data-live-search="true" data-size="auto" data-width="160px";>';
+html_addalarm += '<option value="" label=""></option><option value="gt" label="maior">maior</option><option value="lt" label="menor">menor</option></select></div></div>';
+//div para adição de novo parametro
+html_addalarm += '<div class="row"><div class="col-lg-8 col-md-8">Time  <input type="text" id="time" name="time_val"></input></div></div>'
 
 
 /*Funcao que oferece a adição de alarme*/
@@ -29,14 +29,16 @@ function addAlarme() {
 					var nome = $('#name_alarm').val();
 					var operador = $('#myOp').find(":selected").val();
 					var parametro = $('#parametro').val();
-
+					/*
+					 * Criar variavel para receber o parametro de tempo e modificar a chamada de criação do alarme
+					 */
 					var url_alarm = ip_server + "/add_alarm?";
 					url_alarm += "name=" + nome;
 					url_alarm += "&resource=" + recurso;
 					url_alarm += "&operator=" + operador;
 					url_alarm += "&threshold=" + parametro;
 					url_alarm += "&period=" + 100;
-
+				
 					// Requisicao para adicionar o alarme
 					$.ajax({
 						url : url_alarm,
