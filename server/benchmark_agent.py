a6ef7b284b05e95133f4268685e5b110b4bc3834
DiegoPereira <diegolimapereira@gmail.com>
2014-05-02 17:14:08 -0300

    threads de benchmark

--- server/benchmark_agent.py
+++ server/benchmark_agent.py
@@ -1,15 +1,23 @@
-from telemetry_data import DataHandler
 import json, requests, time
+from telemetry_data import DataHandler
+from benchmark_data import BenchmarkDataHandler
 
 def start_bench_th(projeto):
     dados = DataHandler()
-    dados.start_instance_bench(projeto)
-    while dados.get_benchmark_status != True:
-        time.sleep(15)
-    #pega o resultado e escreve no banco
-    a = open('a.txt','w')
-    a.close()    
-    return 'funfou'
+    bench = BenchmarkDataHandler()
+    dados.start_instance_bench('admin')
 
+    while True:
+        try:
+            if dados.get_benchmark_status('admin') == 'True':
+                break
+            time.sleep(5)
+        except:
+            continue
 
+    read_benchmark = json.loads(dados.get_benchmark('admin')[0])
+    read_benchmark['Host'] = '150.165.15.4'
+#    read_benchmark['Host']='150.165.15.4'
+   # bench.save_data_db(read_benchmark)
+    return read_benchmark
 
