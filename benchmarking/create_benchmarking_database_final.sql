b3a911f913ee24bd710620e84a3aded9fdea99e5
albertofagner <albertofagner.cav@gmail.com>
2014-07-07 15:40:12 -0300

    atualização bd

--- benchmarking/create_benchmarking_database_final.sql
+++ benchmarking/create_benchmarking_database_final.sql
@@ -7,81 +7,76 @@ CREATE TABLE benchmark_history(
 
 CREATE TABLE cpu_table(
      id MEDIUMINT,
-     cpu_average_time FLOAT(5,2),
-     cpu_median_time FLOAT(5,2),
-     cpu_min_time FLOAT(5,2),
-     cpu_max_time FLOAT(5,2),
-     first_quarter_time FLOAT(5,2),
-     second_quarter_time FLOAT(5,2),
-     third_quarter_time FLOAT(5,2),
-     fourth_quarter_time FLOAT(5,2),
+     cpu_average_time FLOAT(7,2),
+     cpu_median_time FLOAT(7,2),
+     cpu_min_time FLOAT(7,2),
+     cpu_max_time FLOAT(7,2),
+     first_quarter_time FLOAT(7,2),
+     second_quarter_time FLOAT(7,2),
+     third_quarter_time FLOAT(7,2),
+     fourth_quarter_time FLOAT(7,2),
      PRIMARY KEY (id),
      FOREIGN KEY (id) REFERENCES benchmark_history(id)
 );
 
 CREATE TABLE mem_table(
      id MEDIUMINT,
-     mem_average_time FLOAT(5,2),
-     mem_median_time FLOAT(5,2),
-     mem_min_time FLOAT(5,2),
-     mem_max_time FLOAT(5,2),
-     first_quarter_time FLOAT(5,2),
-     second_quarter_time FLOAT(5,2),
-     third_quarter_time FLOAT(5,2),
-     fourth_quarter_time FLOAT(5,2),
+     mem_average_time FLOAT(7,2),
+     mem_median_time FLOAT(7,2),
+     mem_min_time FLOAT(7,2),
+     mem_max_time FLOAT(7,2),
+     first_quarter_time FLOAT(7,2),
+     second_quarter_time FLOAT(7,2),
+     third_quarter_time FLOAT(7,2),
+     fourth_quarter_time FLOAT(7,2),
      PRIMARY KEY (id),
      FOREIGN KEY (id) REFERENCES benchmark_history(id)
 );
 
 CREATE TABLE disk_table(
      id MEDIUMINT,
-     disk_average_time FLOAT(5,2),
-     disk_median_time FLOAT(5,2),
-     disk_min_time FLOAT(5,2),
-     disk_max_time FLOAT(5,2),
-     first_quarter_time FLOAT(5,2),
-     second_quarter_time FLOAT(5,2),
-     third_quarter_time FLOAT(5,2),
-     fourth_quarter_time FLOAT(5,2),
+     disk_average_time FLOAT(7,2),
+     disk_median_time FLOAT(7,2),
+     disk_min_time FLOAT(7,2),
+     disk_max_time FLOAT(7,2),
+     first_quarter_time FLOAT(7,2),
+     second_quarter_time FLOAT(7,2),
+     third_quarter_time FLOAT(7,2),
+     fourth_quarter_time FLOAT(7,2),
      PRIMARY KEY (id),
      FOREIGN KEY (id) REFERENCES benchmark_history(id)
 );
 
-INSERT INTO benchmark_history values(1, '2014-02-18', '150.165.15.4');
-INSERT INTO benchmark_history values(2, '2014-02-19', '150.165.15.4');
-INSERT INTO benchmark_history values(3, '2014-02-18', '150.165.15.37');
-INSERT INTO benchmark_history values(4, '2014-02-19', '150.165.15.37');
-INSERT INTO benchmark_history values(5, '2014-02-18', '150.165.15.42');
-INSERT INTO benchmark_history values(6, '2014-02-19', '150.165.15.42');
-INSERT INTO benchmark_history values(7, '2014-02-20', '150.165.15.42');
+INSERT INTO benchmark_history values(1, '2014-02-18', 'compute1');
+INSERT INTO benchmark_history values(2, '2014-02-19', 'compute1');
+INSERT INTO benchmark_history values(3, '2014-02-18', 'compute2');
+INSERT INTO benchmark_history values(4, '2014-02-19', 'compute2');
+INSERT INTO benchmark_history values(5, '2014-02-18', 'compute3');
+INSERT INTO benchmark_history values(6, '2014-02-19', 'compute3');
 
-INSERT INTO cpu_table values(1, 20.5, 19, 10, 30, 15, 20.5, 25, 30);
-INSERT INTO mem_table values(1, 22.5, 21, 12, 32, 17, 22.5, 27, 32);
-INSERT INTO disk_table values(1, 18.5, 17, 8, 28, 13, 18.5, 23, 28);
+INSERT INTO cpu_table values(1, 14027, 13938, 13893, 14390, 13921, 13938, 14029, 14390);
+INSERT INTO mem_table values(1, 4688, 4635, 4616, 5540, 4625, 4635, 4717, 5540);
+INSERT INTO disk_table values(1, 1775, 1764, 1753, 1830, 1758, 1764, 1779, 1830);
 
-INSERT INTO cpu_table values(2, 220.5, 219, 210, 230, 215, 220.5, 225, 230);
-INSERT INTO mem_table values(2, 222.5, 221, 212, 232, 217, 222.5, 227, 232);
-INSERT INTO disk_table values(2, 218.5, 217, 28, 228, 213, 218.5, 223, 228);
+INSERT INTO cpu_table values(2, 15027, 14938, 14893, 15390, 14921, 14938, 15029, 15390);
+INSERT INTO mem_table values(2, 5688, 5635, 5616, 6540, 5625, 5635, 5717, 6540);
+INSERT INTO disk_table values(2, 2775, 2764, 2753, 2830, 2758, 2764, 2779, 2830);
 
-INSERT INTO cpu_table values(3, 320.5, 319, 310, 330, 315, 320.5, 325, 330);
-INSERT INTO mem_table values(3, 322.5, 321, 312, 332, 317, 322.5, 327, 332);
-INSERT INTO disk_table values(3, 318.5, 317, 38, 328, 313, 318.5, 323, 328);
+INSERT INTO cpu_table values(3, 12027, 11938, 11893, 12390, 11921, 11938, 12029, 12390);
+INSERT INTO mem_table values(3, 6688, 6635, 6616, 7540, 6625, 6635, 6717, 7540);
+INSERT INTO disk_table values(3, 3775, 3764, 3753, 3830, 3758, 3764, 3779, 3830);
 
-INSERT INTO cpu_table values(4, 20.5, 19, 10, 30, 15, 20.5, 25, 30);
-INSERT INTO mem_table values(4, 22.5, 21, 12, 32, 17, 22.5, 27, 32);
-INSERT INTO disk_table values(4, 18.5, 17, 8, 28, 13, 18.5, 23, 28);
+INSERT INTO cpu_table values(4, 24027, 23938, 23893, 24390, 23921, 23938, 24029, 24390);
+INSERT INTO mem_table values(4, 2688, 2635, 2616, 3540, 2625, 2635, 2717, 3540);
+INSERT INTO disk_table values(4, 1775, 1764, 1753, 1830, 1758, 1764, 1779, 1830);
 
-INSERT INTO cpu_table values(5, 420.5, 419, 410, 430, 415, 420.5, 425, 430);
-INSERT INTO mem_table values(5, 422.5, 421, 412, 432, 417, 422.5, 427, 432);
-INSERT INTO disk_table values(5, 418.5, 417, 48, 428, 413, 418.5, 423, 428);
+INSERT INTO cpu_table values(5, 14027, 13938, 13893, 14390, 13921, 13938, 14029, 14390);
+INSERT INTO mem_table values(5, 4088, 4035, 4016, 5040, 4016, 4035, 4038, 5040);
+INSERT INTO disk_table values(5, 1575, 1564, 1553, 1630, 1558, 1564, 1579, 1630);
 
-INSERT INTO cpu_table values(6, 820.5, 819, 810, 830, 815, 820.5, 825, 830);
-INSERT INTO mem_table values(6, 822.5, 821, 812, 832, 817, 822.5, 827, 832);
-INSERT INTO disk_table values(6, 818.5, 817, 88, 828, 813, 818.5, 823, 828);
-
-INSERT INTO cpu_table values(7, 620.5, 619, 610, 630, 615, 620.5, 625, 630);
-INSERT INTO mem_table values(7, 622.5, 621, 612, 632, 617, 622.5, 627, 632);
-INSERT INTO disk_table values(7, 618.5, 617, 68, 628, 613, 618.5, 623, 628);
+INSERT INTO cpu_table values(6, 18017, 17928, 17883, 18380, 17911, 17928, 18019, 18380);
+INSERT INTO mem_table values(6, 5088, 5035, 5016, 5940, 5025, 5035, 5117, 5940);
+INSERT INTO disk_table values(6, 1885, 1874, 1863, 1940, 1868, 1874, 1889, 1940);
 
 DELETE FROM cpu_table;
 DELETE FROM mem_table;
